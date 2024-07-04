beb88435422f41287e32feeeaa9d3841df165814
Afaq <afaqali4244@gmail.com>
2024-07-04 08:45:21 +0500

    Project started

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        yellowLight: "#FFAE45",
+        yellowDark: "#E47302",
+        black: "#000000",
+      }
     },
   },
   plugins: [],
