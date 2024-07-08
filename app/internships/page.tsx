a8da07a11c4f57ed3cffc05af19c5e0e603eba72
Afaq <afaqali4244@gmail.com>
2024-07-08 21:58:30 +0500

    Done with changes

---
+++ app/internships/page.tsx
@@ -0,0 +1,11 @@
+import Title from "../components/ui/Title"
+
+const Internships = () => {
+  return (
+    <div className="container">
+        <Title>Internships</Title>
+    </div>
+  )
+}
+
+export default Internships
---
+++ app/workspaces/page.tsx
@@ -0,0 +1,11 @@
+import Title from "../components/ui/Title"
+
+const Workspaces = () => {
+  return (
+    <div className="container">
+        <Title>Workspaces</Title>
+    </div>
+  )
+}
+
+export default Workspaces
