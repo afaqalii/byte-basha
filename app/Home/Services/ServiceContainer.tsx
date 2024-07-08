72bb727ed3b95d748ab8eea6221069f39ff34d3f
Afaq <afaqali4244@gmail.com>
2024-07-08 21:55:53 +0500

    Doen with chnages

--- app/Home/Services/ServiceContainer.tsx
+++ app/Home/Services/ServiceContainer.tsx
@@ -5,7 +5,7 @@ const ServiceContainer = ({ title, services }: ServicesContainerProps) => {
         <div className="max-w-7xl mx-auto pb-12 pt-6">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {services.map((service, index) => (
-                    <div key={index} className="bg-white shadow-md rounded-lg p-6">
+                    <div key={index} className="bg-white shadow-xl rounded-lg p-6 border-yellowLight border-2">
                         <div className="flex items-center mb-4">
                             <div className="text-4xl text-orange-500">{<service.icon />}</div>
                             <h2 className="text-xl font-semibold ml-4">{service.title}</h2>
---
+++ app/training/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../components/ui/Title'
+
+const Training = () => {
+  return (
+    <div className='container'>
+        <Title>Training</Title>
+    </div>
+  )
+}
+
+export default Training
