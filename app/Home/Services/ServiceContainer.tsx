c500e84546630683d4e2aa95137985b59acd4947
Afaq <afaqali4244@gmail.com>
2024-08-19 00:28:20 +0500

    added tranition to services components

--- app/Home/Services/ServiceContainer.tsx
+++ app/Home/Services/ServiceContainer.tsx
@@ -6,7 +6,7 @@ const ServiceContainer = ({ title, services }: ServicesContainerProps) => {
         <div className="max-w-7xl mx-auto pb-12 pt-6">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {services.map((service, index) => (
-                    <Link href={service.href} key={index} className="bg-white shadow-xl rounded-lg p-6 border-yellowLight border-2">
+                    <Link href={service.href} key={index} className="bg-white shadow-xl rounded-lg p-6 border-yellowLight border-2 hover:scale-105 transition">
                         <div className="flex items-center mb-4">
                             <div className="text-4xl text-orange-500">{<service.icon />}</div>
                             <h2 className="text-xl font-semibold ml-4">{service.title}</h2>
