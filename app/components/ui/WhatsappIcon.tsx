73d252de0efdfc500792079b337ecbe13d93e4a0
Afaq <afaqali4244@gmail.com>
2024-08-27 17:10:04 +0500

    Ddone with changes whatsapp icon

--- app/components/ui/WhatsappIcon.tsx
+++ app/components/ui/WhatsappIcon.tsx
@@ -4,7 +4,7 @@ export const WhatsAppIcon = () => {
     return (
         <a 
             href="https://wa.me/923157021585" 
-            className="fixed bottom-6 right-6 z-20 bg-white rounded-full p-1 cursor-pointer bounce-animation"
+            className="fixed bottom-24 right-6 z-20 bg-white rounded-full p-1 cursor-pointer bounce-animation"
             rel="noopener noreferrer"
         >
             <RiWhatsappFill className="text-green-700 h-[50px] w-[50px]" />
